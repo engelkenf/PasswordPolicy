40c5e625bd9e3812ce2e020cd21acc0e99a273be
Roman Engelke <engelke.roman@gmail.com>
2021-09-10 11:47:08 +0200

    add digit check incl test

--- src/main/java/PasswordPolicy.java
+++ src/main/java/PasswordPolicy.java
@@ -4,12 +4,18 @@ public class PasswordPolicy {
         String testPassWord = "Hallo";
 
         System.out.println("Das gewählte Passwort lautet: " + testPassWord);
-        System.out.println("Ist das Password lang genug?" + checkPasswordLength(testPassWord));
+        System.out.println("Ist das Password lang genug? " + checkPasswordLength(testPassWord));
+        System.out.println("Enthält das Password Ziffern? " + checkPasswordContainsDigits(testPassWord));
+
 
     }
 
     public static boolean checkPasswordLength(String password){
         if (password.length()<6) return false;
         else return true;
+    }
+
+    public static boolean checkPasswordContainsDigits(String password){
+        return password.matches(".*[0-9].*");
     }
 }
--- src/test/java/PasswordPolicyTest.java
+++ src/test/java/PasswordPolicyTest.java
@@ -7,17 +7,32 @@ public class PasswordPolicyTest {
 
     @Test
     @DisplayName("checkPasswordLengthTest")
-    public static void checkPasswordLengthTest(){
+    public void checkPasswordLengthTest(){
 
         // GIVEN
         String passwordLongEnough = "Test123!";    // happy path: has to be longer than 6 chars
         String passwordTooShort = "hallo";         // negative path: less than 6 chars
 
         // WHEN
-        boolean passwordLengthChecktresult = PasswordPolicy.checkPasswordLength(passwordLongEnough);
+        boolean passwordLengthCheckResult = PasswordPolicy.checkPasswordLength(passwordLongEnough);
 
         // THEN
-        Assertions.assertTrue(passwordLengthChecktresult);
+        Assertions.assertTrue(passwordLengthCheckResult);
+    }
+
+    @Test
+    @DisplayName("checkPasswordContainsDigitsTest")
+    public void checkPasswordContainsDigitsTest(){
+
+        // GIVEN
+        String passwordContainsDigits = "Test123!"; // happy path: it contains digits
+        String passwordWithoutDigits = "Hallo";     // negative path
+
+        // WHEN
+        boolean passwordDigitCheckResult = PasswordPolicy.checkPasswordContainsDigits(passwordContainsDigits);
+
+        // THEN
+        Assertions.assertTrue(passwordDigitCheckResult);
     }
 
 }
